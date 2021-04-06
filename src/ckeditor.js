984c7c4fbfd78b544081414230b1e3ea82c3d85b
Merisha Maharjan <merisha@studyinfocentre.com>
2021-04-06 08:50:23 +0545

    added upto heading 6

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -32,6 +32,9 @@ import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformatio
 
 import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize";
 
+import HeadingButtonsUI from "@ckeditor/ckeditor5-heading/src/headingbuttonsui";
+import ParagraphButtonUI from "@ckeditor/ckeditor5-paragraph/src/paragraphbuttonui";
+
 export default class ClassicEditor extends ClassicEditorBase {}
 
 // Plugins to include in the build.
@@ -60,13 +63,22 @@ ClassicEditor.builtinPlugins = [
 	TableToolbar,
 	TextTransformation,
 	ImageResize,
+	HeadingButtonsUI,
+	ParagraphButtonUI,
 ];
 
 // Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
 		items: [
-			"heading",
+			// "heading",
+			"paragraph",
+			"heading1",
+			"heading2",
+			"heading3",
+			"heading4",
+			"heading5",
+			"heading6",
 			"|",
 			"bold",
 			"italic",
@@ -117,6 +129,51 @@ ClassicEditor.defaultConfig = {
 	},
 	table: {
 		contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"],
+	},
+	heading: {
+		options: [
+			{
+				model: "paragraph",
+				title: "Paragraph",
+				class: "ck-heading_paragraph",
+			},
+			{
+				model: "heading1",
+				view: "h1",
+				title: "Heading 1",
+				class: "ck-heading_heading1",
+			},
+			{
+				model: "heading2",
+				view: "h2",
+				title: "Heading 2",
+				class: "ck-heading_heading2",
+			},
+			{
+				model: "heading3",
+				view: "h3",
+				title: "Heading 3",
+				class: "ck-heading_heading3",
+			},
+			{
+				model: "heading4",
+				view: "h4",
+				title: "Heading 4",
+				class: "ck-heading_heading4",
+			},
+			{
+				model: "heading5",
+				view: "h5",
+				title: "Heading 5",
+				class: "ck-heading_heading5",
+			},
+			{
+				model: "heading6",
+				view: "h6",
+				title: "Heading 6",
+				class: "ck-heading_heading6",
+			},
+		],
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
 	language: "en",
